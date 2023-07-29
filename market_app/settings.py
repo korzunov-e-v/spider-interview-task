f0cdeff3c058c3d1e90bb779ab502494eedca4cb
Evgeniy Korzunov <korzunov.e.v@gmail.com>
2023-07-29 16:58:54 +0500

    security fix

--- market_app/settings.py
+++ market_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9@mkwt7(ao&--7+0ke!11p6vf4v0ddvz^)pxgo#232fsgv5rhb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
